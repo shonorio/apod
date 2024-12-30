b55fbfb6627eaca56fe49661500ec1d31fc26392
Solli Honorio <shonorio@gmail.com>
2024-12-30 10:05:22 -0300

    feat(storage): update StorageProvider methods to handle Map<String, dynamic> for improved data structure

--- lib/app/core/storage_provider/storage_provider.dart
+++ lib/app/core/storage_provider/storage_provider.dart
@@ -1,11 +1,11 @@
 abstract class StorageProvider {
   /// Stores an item with the given [id] and [value]
   /// Returns [true] if the operation was successful
-  Future<bool> create(String id, String value);
+  Future<bool> create(String id, Map<String, dynamic> value);
 
   /// Retrieves all stored items
   /// If [id] is provided, retrieves only the item with the given [id]
-  Future<List<String>> fetch({String? id});
+  Future<List<Map<String, dynamic>>> fetch({String? id});
 
   /// Removes an item with the given [id]
   /// Returns [true] if the operation was successful
