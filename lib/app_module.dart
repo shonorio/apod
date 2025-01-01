b7f513acf50a6f22652519f62fb98e83c011fe96
Solli Honorio <shonorio@gmail.com>
2025-01-01 10:53:16 -0300

    feat(app): integrate storage provider and favorites repository into AppModule for improved dependency management

--- lib/app_module.dart
+++ lib/app_module.dart
@@ -1,4 +1,7 @@
+import 'package:apod/app/core/storage_provider/shared_preferences_storage.dart';
+import 'package:apod/app/core/storage_provider/storage_provider.dart';
 import 'package:apod/app/features/apod/ui/apod_page.dart';
+import 'package:apod/app/features/favorites/domain/repository/favorites_repository.dart';
 import 'package:apod/app/features/favorites/ui/favorites_page.dart';
 import 'package:apod/app/features/favorites/ui/favorites_page_controller.dart';
 import 'package:apod/app/features/home/ui/page/home_page.dart';
@@ -7,7 +10,10 @@ import 'package:flutter_modular/flutter_modular.dart';
 class AppModule extends Module {
   @override
   void binds(Injector i) {
-    i.add(FavoritesPageController.new);
+    i
+      ..add<StorageProvider>(SharedPreferencesStorage.new)
+      ..add<FavoritesRepository>(FavoritesRepository.new)
+      ..add<FavoritesPageController>(FavoritesPageController.new);
   }
 
   @override
