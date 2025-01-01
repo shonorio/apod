7cd89c92c90b90d2737139e8c6083a1dc3061e4c
Solli Honorio <shonorio@gmail.com>
2025-01-01 20:24:02 -0300

    feat(error_state_widget): add ErrorStateWidget for displaying error messages and retry functionality

---
+++ lib/app/features/apod/ui/widget/error_state_widget.dart
@@ -0,0 +1,44 @@
+import 'package:apod/app/core/extensions/build_context.dart';
+import 'package:flutter/material.dart';
+
+class ErrorStateWidget extends StatelessWidget {
+  const ErrorStateWidget({
+    super.key,
+    required this.onPressed,
+    required this.subtitle,
+    required this.title,
+  });
+
+  final String title;
+  final String subtitle;
+  final VoidCallback? onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          title,
+          style: context.textTheme.titleLarge?.copyWith(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        const SizedBox(height: 8),
+        Text(
+          subtitle,
+          style: context.textTheme.bodyMedium,
+          textAlign: TextAlign.center,
+        ),
+        const SizedBox(height: 16),
+        SizedBox(
+          width: 200,
+          child: ElevatedButton(
+            onPressed: onPressed,
+            child: const Text('Retry'),
+          ),
+        ),
+      ],
+    );
+  }
+}
